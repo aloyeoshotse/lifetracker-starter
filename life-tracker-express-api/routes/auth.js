2aa3cbb5b24da9aa60e460f1c8bef99a1ba4898e
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-09 19:05:25 -0700

    updated auth.js

--- life-tracker-express-api/routes/auth.js
+++ life-tracker-express-api/routes/auth.js
@@ -1,27 +1,28 @@
-import express from "express"
-const router = express.Router();
+const express = require('express');
+const User = require('../models/user');
+const router = express.Router()
 
 router.post('/login', async(req,res, next) => {
-  try{
-      //take email and passwords and attempt to authenticate
-      const user = await User.login(req.body);
-      return res.status(200).json({ user });
-  }
-  catch(err) {
-      next(err)
-  }
+    try{
+        //take email and passwords and attempt to authenticate
+        const user = await User.login(req.body);
+        return res.status(200).json({ user });
+    }
+    catch(err) {
+        next(err)
+    }
 })
 
 router.post('/register', async(req,res, next) => {
-  try{
-      /*take user email, password, rsvp status, and number of guests 
-          and creat a new user in database */
-      const user = await User.register(req.body);
-      return res.status(201).json({ user });
-  }
-  catch(err) {
-      next(err)
-  }
+    try{
+        /*take user email, password, rsvp status, and number of guests 
+            and creat a new user in database */
+        const user = await User.register(req.body);
+        return res.status(201).json({ user });
+    }
+    catch(err) {
+        next(err)
+    }
 })
 
-export default router;
+module.exports = router;
