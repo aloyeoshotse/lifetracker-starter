92f96bfd5c78035c5a4d6f3d0768c38b19c9c296
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-09 18:38:27 -0700

    worked on navbar

--- life-tracker-ui/src/components/Navbar/Navbar.jsx
+++ life-tracker-ui/src/components/Navbar/Navbar.jsx
@@ -6,25 +6,27 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import  './Navbar.css'
 
 export function NavBar() {
   return (
     <Box>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <img href={'/'}f id="logo" src="/src/lifetracker-logo.png"/>
+          {/* <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+          <MenuIcon />
+          </IconButton> */}
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" sx={{marginLeft: "2cm"}}>Login</Button>
+          <Button color="inherit">Register</Button>
         </Toolbar>
       </AppBar>
     </Box>
